24669d68cbf86e7f129d2a8b436357a48a539bfd
Sirisk <adamchrisk@gmail.com>
2023-10-27 10:26:18 +0200

    user create views endpoint

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.serializers import ModelSerializer, EmailField, CharField
+from .models import CustomUser
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -9,3 +11,28 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         # Add custom claims
         token["fav_color"] = user.fav_color
         return token
+
+
+class CustomUserSerializer(ModelSerializer):
+    """
+    Currently unused in preference of the below.
+    """
+
+    email = EmailField(required=True)
+    username = CharField()
+    password = CharField(min_length=8, write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ("email", "username", "password")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        instance = self.Meta.model(
+            **validated_data
+        )  # as long as the fields are the same, we can just use this
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
