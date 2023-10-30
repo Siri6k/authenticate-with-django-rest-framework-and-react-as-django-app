68c20d3279be6c67ddd1ec42dc43dfd7a23745e3
Sirisk <adamchrisk@gmail.com>
2023-10-30 11:26:09 +0200

    login form submit

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Login () {
-  
-        return (
+    const [username , setUsername] = useState("")
+    const [password , setPassword] = useState("")
+
+    const handleUsername = (event) => {
+        setUsername(event.target.value);
+    }
+    const handlePassword = (event) => {
+        setPassword(event.target.value);
+    }
+
+   const handleSubmit = (event) => {
+        alert('A username and password was submitted: ' + username + " " + password);
+        event.preventDefault();
+    }
+    return (
             <div>
                 <h2>Login page</h2>
-            </div>
-        );
-    }
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username:
+                    <input name="username" type="text" value={username} onChange={handleUsername}/>
+                </label>
+                <label>
+                    Password:
+                    <input name="password" type="password" value={password} onChange={handlePassword}/>
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    );
+}
 
 export default Login;
